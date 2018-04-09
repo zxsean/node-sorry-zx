8afe4c312b30724953931c735ec55a90267d64a8
ZeaLotSean <zxsean@126.com>
2018-04-09 18:15:01 +0800

    [+]提交初版.

---
+++ controller/loadjsonconfig.js
@@ -0,0 +1,39 @@
+function loadjsonconfig() {
+    // json处理
+    var fs = require('fs');
+    var file = "./routes/config.json";
+    console.log("读取配置:%s", file)
+    var config = JSON.parse(fs.readFileSync(file));
+
+    // console.log(config);
+    const __strtemplate = "<a href=\"%s\" class=\"w3-bar-item w3-button\">%s</a>";
+
+    const __strtemplate2 =
+        "<p>" +
+        "<label class=\"w3-text-blue\">" +
+        "<b>%s</b>" +
+        "</label>" +
+        "<input class=\"w3-input w3-border\" name=\"first\" type=\"text\" placeholder=\"%s\">" +
+        "</p>";
+
+    // 格式化
+    var util = require("util");
+
+    var configStr = new Object();
+    configStr._tempStr = "";
+    configStr._tempStr2 = "";
+
+    // 遍历
+    for (var value in config) {
+        console.log(config[value]);
+        configStr._tempStr += util.format(__strtemplate, config[value].path, config[value].name);
+
+        for (var i in config[value].talk) {
+            console.log("=====" + config[value].talk[i] + "=====" + i.toString());
+            configStr._tempStr2 += util.format(__strtemplate2, i, config[value].talk[i]);
+            console.log("____" + configStr._tempStr2);
+        }
+    }
+}
+
+module.exports = loadjsonconfig;
