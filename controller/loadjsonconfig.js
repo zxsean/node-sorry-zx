c46c0453e8991475ae2a22bd469e05976f28ed79
ZeaLotSean <zxsean@126.com>
2018-04-10 16:56:34 +0800

    [*]修改模块流程.

--- controller/loadjsonconfig.js
+++ controller/loadjsonconfig.js
@@ -1,3 +1,7 @@
+// 格式化
+const util = require("util");
+const fs = require('fs');
+
 const __strtemplate_sidebar_items = "<a href=\"%s\" class=\"w3-bar-item w3-button\">%s</a>";
 
 const __strtemplate_container_items =
@@ -10,44 +14,46 @@ const __strtemplate_container_items =
 
 const file = "./public/configs/config.json";
 
-var jsonConfig = new Object();
+// 读取Json配置
+function LoadJsonConfig() {
+    // 定义配置
+    this.jsonConfig = new Object();
 
-function loadjsonconfig() {
-    // 格式化
-    var util = require("util");
+    // 重读配置
+    this.LoadConfig = function () {
+        jsonConfig = new Object();
+        jsonConfig.sidebar_items = "";
 
-    jsonConfig.sidebar_items = "";
+        // json处理
+        console.log("读取配置:%s", file)
+        var config = JSON.parse(fs.readFileSync(file));
+        // console.log(config);
 
-    // json处理
-    var fs = require('fs');
+        // 遍历
+        for (var value in config) {
+            // 创建边栏
+            // console.log(config[value]);
+            jsonConfig.sidebar_items += util.format(__strtemplate_sidebar_items, config[value].path, config[value].name);
 
-    console.log("读取配置:%s", file)
-    var config = JSON.parse(fs.readFileSync(file));
-    // console.log(config);
+            // 创建对话
+            jsonConfig[value] = new Object();
+            jsonConfig[value].container_items = "";
 
-    // 遍历
-    for (var value in config) {
-        // 创建边栏
-        // console.log(config[value]);
-        jsonConfig.sidebar_items += util.format(__strtemplate_sidebar_items, config[value].path, config[value].name);
+            jsonConfig[value].title = config[value].title;
+            jsonConfig[value].tips = config[value].tips;
 
-        // 创建对话
-        jsonConfig[value] = new Object();
-        jsonConfig[value].container_items = "";
-
-        jsonConfig[value].title = config[value].title;
-        jsonConfig[value].tips = config[value].tips;
-
-        for (var i in config[value].talk) {
-            // console.log(config[value].talk[i]);
-            jsonConfig[value].container_items += util.format(__strtemplate_container_items, i, config[value].talk[i]);
-            // console.log(jsonConfig[value].container_items + "___" + value);
+            for (var i in config[value].talk) {
+                // console.log(config[value].talk[i]);
+                jsonConfig[value].container_items += util.format(__strtemplate_container_items, i, config[value].talk[i]);
+                // console.log(jsonConfig[value].container_items + "___" + value);
+            }
         }
+
+        this.jsonConfig = jsonConfig;
+
+        console.log("读取配置完成");
     }
-    
-    console.log("读取配置完成");
 }
 
 // 导出
-module.exports = loadjsonconfig;
-module.exports.jsonConfig = jsonConfig;
+module.exports = LoadJsonConfig;
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,34 @@
 var express = require('express');
 var router = express.Router();
-const loadjsonconfig = require('../controller/loadjsonconfig');
+const LoadJsonConfig = require('../controller/loadjsonconfig');
+const loadjsonconfig = new LoadJsonConfig();
 const Render = require('../controller/render');
 
-var _temp = new loadjsonconfig();
-configStr = loadjsonconfig.jsonConfig;
+// 读取
+loadjsonconfig.LoadConfig();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.redirect('./sorry/');
 });
 
+// 重读配置
+router.get('/reloadconfig', function (req, res, next) {
+  console.log("重读配置");
+  loadjsonconfig.LoadConfig();
+  console.log("重读配置完成");
+  res.redirect('./sorry/');
+})
+
 //选择模板页面
 router.get('/:name', function (req, res, next) {
-  var _tempConfig = configStr[req.params.name];
+  // 定义一个临时字典
+  var _tempConfig = loadjsonconfig.jsonConfig[req.params.name];
 
   res.render('index',
     {
       title: _tempConfig.title,
-      sidebar_items: configStr.sidebar_items,
+      sidebar_items: loadjsonconfig.jsonConfig.sidebar_items,
       container_items: _tempConfig.container_items,
       name: req.params.name,
       tips: _tempConfig.tips,
